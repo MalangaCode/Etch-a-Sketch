832fa3d36a3eb8d7957e5f673fa63a8b1607d7f0
luis <102092058+MalangaCode@users.noreply.github.com>
2023-02-28 14:52:20 -0500

    Add function remove canvas
    remove the container of the divs to clean the space and create
    the new layout

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function makeNDivs(n) {
- 
+    removeCanvas();
     createCanvas(n);
 
     for (let i = 0; i < n**2; i++) {
@@ -20,5 +20,9 @@ function createCanvas(n) {
 
     canvas.style.cssText = 
     `grid-template-columns: repeat(${n}, 1fr); grid-template-rows: repeat(${n}, 1fr);`
+}
 
+function removeCanvas() {
+    const element = document.getElementById("canvas");
+    element.remove();
 }
