b2acac6ef8fbfb217337532065551fc6a65ffb01
luis <102092058+MalangaCode@users.noreply.github.com>
2023-02-28 14:46:12 -0500

    Add createCanvas function
    this function create the container for the desired N divs

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 function makeNDivs(n) {
-    
-    for (let i = 0; i < n; i++) {
-        const container = document.querySelector('#sketchPad');
+ 
+    createCanvas(n);
+
+    for (let i = 0; i < n**2; i++) {
+        const container = document.querySelector('#canvas');
         const createDiv = document.createElement('div'); 
 
         createDiv.classList.add('pixel');
         container.appendChild(createDiv);  
     }
+}
+
+function createCanvas(n) {
+
+    const container = document.querySelector('#sketchPad');
+    const createDiv = document.createElement('div'); 
+    createDiv.setAttribute('id', 'canvas');
+    container.appendChild(createDiv); 
+
+    canvas.style.cssText = 
+    `grid-template-columns: repeat(${n}, 1fr); grid-template-rows: repeat(${n}, 1fr);`
+
 }
